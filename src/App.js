9b0e672228c5c435eebf0ec95963f5faa7ff7f83
dagopia <tdaguti@gmail.com>
2023-08-22 06:12:59 +0300

    updated.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ChartComponent from './componenet/ChartComponent';
+import { Chart } from 'react-chartjs-2';
+import DateComponent from './componenet/FormatDate';
+import { useEffect, useState } from 'react';
+import LineChart from './componenet/LineChart';
+import PieChart from './componenet/PieChart';
+import Charts from './componenet/Charts';
+import { Box, Card } from '@mui/material';
 function App() {
+
+
+  const [posts, setPosts] = useState([]);
+   useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
+  const [chartData, setChartData] = useState({
+    // ...chart data
+  });
+  const currentDate = new Date();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Charts />
+      <DateComponent date={currentDate} />
+      return (
+   <Card sx={{bgcolor:'#13827E'}} >
+      {posts.map((post) => {
+         return (
+            <div className="post-card" key={post.id}>
+               <h2 className="post-title">{post.title}</h2>
+               <p className="post-body">{post.body}</p>
+              
+            </div>
+         );
+      })}
+   </Card>
+   );
     </div>
   );
 }
